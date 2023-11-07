3bd617ce5a86ab2ccfe7b79d674215c33dd652be
Konstantin <aziimov1999123@gmail.com>
2023-11-07 15:45:31 +0300

    fixed data

--- src/components/Friends/FriendList.jsx
+++ src/components/Friends/FriendList.jsx
@@ -4,22 +4,18 @@ import PropTypes from 'prop-types';
 export const FriendList = ({ friends }) => {
   // console.log(friends);
 
-  return (
-    <div className={css.friends}>
-      <ul className={css.friends__list}>
-        {friends.map(({ avatar, name, isOnline, id }) => (
-          <li className={css.friends__item} key={id}>
-            <span className={`${css.friends__status} ${css[isOnline]}`}>
-              {isOnline}
-            </span>
-            <img className={css.avatar} src={avatar} alt={name} />
-            <p className={css.friends__name}>{name}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+  const Friend = ({ avatar, name, isOnline }) => {
+    return (
+      <li className={css.friends__item}>
+        <span className={`${css.friends__status} ${isOnline ? css.online : css.offline}`}>
+          {isOnline ? 'Online' : 'Offline'}
+        </span>
+        <img className={css.avatar} src={avatar} alt={name} />
+        <p className={css.friends__name}>{name}</p>
+      </li>
+    );
+  };
+}
 
 FriendList.propTypes = {
   avatar: PropTypes.string.isRequired,
